662b2e8050758def657809477de9b91c1be3f9bf
ElDanto <pavlovian.ch@gmail.com>
2024-06-16 20:16:13 +0300

    route homework

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,16 @@ Route::get('/', function () {
 Route::get('/my_page', function (){
     return 'this is my page';
 });
+
+Route::get('/about', function () {
+    return 'Hi my name is Daniil';
+});
+
+Route::get('/contact', function (){
+    return 'this is my contact page';
+});
+
+Route::get('/city', function() {
+    return 'this is my city page';
+});
+
